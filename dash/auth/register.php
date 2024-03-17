eaf22ae66bdfe6b5fd296b18d5338a1c8d5e6cc5
Michael Erameh <eramehmichael2000@gmail.com>
2024-03-17 22:02:08 +0100

    change

--- dash/auth/register.php
+++ dash/auth/register.php
@@ -104,26 +104,145 @@ if (isset($_POST["submit"])) {
                 //Content
                 $mail->isHTML(true); //Set email format to HTML
                 $mail->Subject = 'Successful Registration';
-                $mail->Body = '<html><head></head></head>
-<body style="background-color: #474d80; padding: 45px;">
-    <div>
-        <img style="position:relative; left:35%;" src="https://tradovex.com/images/logo.png">
-        <h3 style="color: black;">Mail From support@tradovex.com - Successful Registration</h3>
+                $mail->Body = '
+                <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Welcome to Tradovex!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="css/style.css" />
+    <style>
+    * {
+        padding: 0;
+        margin: 0;
+    }
+
+    body {
+        background-color: #fff;
+        height: 100%;
+        width: 100%;
+    }
+
+    .section-body {
+        color: #1a1a1a;
+        font-size: 16px;
+        font-family: Aileron;
+        font-weight: 400;
+        line-height: 22px;
+        word-wrap: break-word;
+        padding-bottom: 20px;
+    }
+
+    .section-header {
+        color: #1a1a1a;
+        font-size: 18px;
+        font-family: Aileron;
+        font-weight: 700;
+        word-wrap: break-word;
+    }
+
+    .footer-link {
+        color: black;
+        font-size: 12px;
+        font-family: Aileron;
+        font-weight: 400;
+        text-decoration: underline;
+        word-wrap: break-word;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="d-flex justify-content-center">
+        <img src="tradovexlogo.png" alt="logo" />
     </div>
-    <div style="color: #ffff;"><hr/>
-        <h3>Dear ' . $first_name . '</h3>
-        <p>You are welcome to tradovex, an automated  online trading  platform made so even investors with zero trading experience  are successfully making profit </p>
-        <h5>Kindly click the button below to log in and proceed to KYC verification</h5>
-        <a style="background-color:#060c39;color:#ffff; padding:15px; text-decoration:none;border-radius: 10px;font-size: 20px;" href="https://tradovex.com/dash/auth/login.php" class="btn btn primary">Sign in</a>
+    <div style="background-color: #09055e;"
+        class="header d-flex flex-column justify-content-center align-items-center text-center py-5">
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a style="color: #fff;" class="nav-link active" aria-current="page" href="#">My account</a>
+            </li>
+            <li class="nav-item">
+                <a style="color: #fff;" class="nav-link" href="#">FAQ</a>
+            </li>
+            <li class="nav-item">
+                <a style="color: #fff;" class="nav-link" href="#">About Us</a>
+            </li>
+        </ul>
 
-        <h5>Note : the details in this email should not be disclosed to anyone</h5>
+    </div>
+    <div style="position: relative; top: -30px, padding-top: 20px;"
+        class="container d-flex flex-column justify-content-center px-5">
+        <h4 style="font-size: 20px; padding-top: 15px; padding-bottom: 5px;" class="section-header">Hey there, welcome
+            to the community!</h4>
+        <p style="font-size: 17px; font-weight: 200;" class="section-body">
+            We’re here to help make your trades easy, simple, and profitable!
 
-    </div><hr/>
-        <div style="background-color: white; color: black;">
-            <h3 style="color: black;">support@tradovex<sup>TM</sup> - Phone : +17577423555</h3>
+            Since 2017, we have been revolutionizing the way people trade cryptocurrencies. By providing an intuitive
+            and reliable platform with powerful tools, we help people of all skill levels make the most money in the
+            shortest amount of time – without having to stay awake or be chained to a computer.
+        </p>
+        <div class="container">
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <img src="mail-images/smart.png">
+                        <h5 style="font-weight: 800;">SmartTrade</h5>
+                        <p>
+                            An exclusive 3Commas feature that pairs with TradingView indicators that lets users view
+                            multiple signals from one place.
+                        </p>
+                    </div>
+                    <div class="col">
+                        <img src="mail-images/dca.png">
+                        <h5 style="font-weight: 800;">DCA Bot</h5>
+                        <p>Invest at preset intervals to reduce the entry price of a position over time and mitigate
+                            volatility risk.</p>
+                    </div>
+                    <div class="col">
+                        <img src="mail-images/grid.png">
+                        <h5 style="font-weight: 800;">GRID Bot</h5>
+                        <p>Small price movements drive HODLers crazy, use the bot to follow the money!</p>
+                    </div>
+                </div>
+            </div>
         </div>
+        <button style="background-color: #09055e; color: #fff;" class="btn">Lets get it started!</button>
+        <div class="container">
+            <p style="padding-top: 15px;" class="section-body">
+                Thank you once again for choosing to join our community. We\'re honored to be a part
+                of your journey and look forward to a successful collaboration.
+                <br /><br />
 
-</body></html>
+                Best Regards,<br />
+                Tradovex Team
+            </p>
+        </div>
+        <div class="footer">
+            <div class="d-flex justify-content-center">
+                <a class="p-2" href=""><img src="mail-images/facebook.png" alt="" /></a>
+                <a class="p-2" href=""><img src="mail-images/instagram.png" alt="" /></a>
+                <a class="p-2" href=""><img src="mail-images/linkedin.png" alt="" /></a>
+            </div>
+            <div>
+                <p class="section-body text-center">
+                    ©2017 Tradovex All rights reserved
+                </p>
+                <div class="d-flex justify-content-center">
+                    <a class="p-2 footer-link" href="">Unsubscribe</a>
+                    <a class="p-2 footer-link" href="">Terms & Privacy</a>
+                    <a class="p-2 footer-link" href="">Contact Us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
 
 ';
 
--- dash/user-area/includes/connection.php
+++ dash/user-area/includes/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("mocha3040.mochahost.com", "exposur2_speniet", "G3vYpL6gBl", "exposur2_speniet");
+$con = mysqli_connect("localhost", "trad_tradovex", "G3vYpL6gBl", "trad_tradovex");
 //$con = mysqli_connect("localhost", "root", "", "speniet");
 
 // if($con){
