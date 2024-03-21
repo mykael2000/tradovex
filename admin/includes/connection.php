567bda325811d04d0f8c5a86643eaf658bf0f97a
Michael Erameh <eramehmichael2000@gmail.com>
2024-03-21 02:32:31 +0100

    s

--- admin/includes/connection.php
+++ admin/includes/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("mocha3040.mochahost.com", "exposur2_tradovex", "G3vYpL6gBl", "exposur2_tradovex");
+$conn = mysqli_connect("localhost", "trad_tradovex", "G3vYpL6gBl", "trad_tradovex");
 //$conn = mysqli_connect("localhost", "root", "", "tradovex");
 
 // if($con){
