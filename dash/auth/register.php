4bdbe9c087dd113566fe9cd680c3c237a78e6fa8
Michael Erameh <eramehmichael2000@gmail.com>
2024-03-22 01:52:55 +0100

    s

--- dash/auth/register.php
+++ dash/auth/register.php
@@ -91,7 +91,7 @@ if (isset($_POST["submit"])) {
                 $mail->Host = 'srv46404747.ultasrv.net'; //Set the SMTP server to send through
                 $mail->SMTPAuth = true; //Enable SMTP authentication
                 $mail->Username = 'support@tradovex.com'; //SMTP username
-                $mail->Password = 'Bitcoin123'; //SMTP password
+                $mail->Password = 'trad123@214VEX'; //SMTP password
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
                 $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
